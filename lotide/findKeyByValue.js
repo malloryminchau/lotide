b0c720a4d511d7cc1a847dbc0b8e0f4af5cc561a
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-21 19:20:04 +0000

    findKeyByValue function activity for tv shows

---
+++ lotide/findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(shows, value) {
+  return Object.keys(shows).find(key => shows[key] === value);
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
