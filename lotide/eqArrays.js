5c1c1042af7caebc85feebdd708490411ac7255c
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-26 14:11:40 +0000

    refactor eqArrays and added eqArraysTest to test folder

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
@@ -15,11 +9,5 @@ const eqArrays = function(firstArray, secondArray) {
   }
   return true;
 }
+module.exports = eqArrays;
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); // => should PASS
