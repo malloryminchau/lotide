e076c16b3bd9bb13a638373a969eafba80585426
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-26 17:35:54 +0000

    changed the tailTest file to use mocha test functions

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,11 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), ([2, 3]));
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), ([]));
+  });
+});
