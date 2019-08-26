133ccd4f70ebcc2c5c3f4f89af9ae4233845ae51
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-26 17:49:43 +0000

    added more functions to the index file

--- lotide/index.js
+++ lotide/index.js
@@ -1,9 +1,17 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual= require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = requore('./eqArrays');
+
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays
 };
